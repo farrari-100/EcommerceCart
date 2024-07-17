9cc16b77f795f0361a24e831beb5b6b5603f0a7a
farrari-100 <devendrakumarjnv6@gmail.com>
2024-07-17 14:54:38 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <h4 className='px-6' style={{ fontWeight: 400, backgroundColor:'white', color:'red'}}><span style={{fontWeight: 400,color:'black'}}>Created By: </span>Amit Kumar (ak7rathaur@gmail.com)</h4>
+    </>
+  )
+}
+
+export default Footer;
